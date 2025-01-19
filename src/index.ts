2b9049f28130e1a339a4aaf2025792a419e445af
sadanandkaji <sadanandkaji1@gmail.com>
2025-01-19 17:15:26 +0530

    second server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express';
 import {usermodel} from "./db"
-import{ Jwt }from "jsonwebtoken"
+import Jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const JWT_SECRET="sadanan1234"
 const app=express();
 app.use(express.json())
+
+
 app.post("/api/v1/signup", async (req,res)=>{
      const username=req.body.username;
      const password=req.body.password;
   
+     const hashedpassword= await bcrypt.hash(password,4);
 
      await usermodel.create({
           username:username,
-          password:password
+          password:hashedpassword
      })
      res.json({
           message:"you have signed up"
@@ -18,46 +25,29 @@ app.post("/api/v1/signup", async (req,res)=>{
      
 })
 
-app.post("api/v1/signin", async (req,res)=>{
+app.post("/api/v1/signin", async (req,res)=>{
      const username=req.body.username;
      const password=req.body.username;
-
-     const founduser= await usermodel.findOne({
-          username:username,
-         
+    
+     const founduser  = await usermodel.findOne({
+          username:username
      })
+// @ts-ignore
+     const decryptedpassword= await bcrypt.compare(password,founduser.password)
   
-     if(founduser){
-          
-          
-
+     if(decryptedpassword){
+         const usertoken= Jwt.sign({
+          //@ts-ignore
+         id:founduser._id.toString()
+     },JWT_SECRET)
+     res.json({
+          usertoken:usertoken
+          })
      }
-
-})
-
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
-
-})
-app.post("api/v1/signup",(req,res)=>{
      
+
 })
+
+
 
 app.listen(3000);
