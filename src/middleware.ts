5f68eb32ada267467e2fb6d5751ba4fd1964d245
sadanandkaji <sadanandkaji1@gmail.com>
2025-01-19 21:16:11 +0530

    a

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ import { JWT_SECRET } from "./config";
 export function usermiddleware(req:Request,res:Response,next:NextFunction){
     
  const token=req.headers["authorization"];
+ //@ts-ignore
  const  decodedpassword=Jwt.verify(token as string ,JWT_SECRET)
 
    if(decodedpassword){
